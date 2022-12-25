133e1c4364c1fea326e356037188902a9b19459e
Sherydan <tobar.luis.c@gmail.com>
2022-12-24 22:19:38 -0300

    validation helper functions

--- index.js
+++ index.js
@@ -11,4 +11,27 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+const checkIfFileExists = async (filePath) => {
+    try {
+        //check if exists
+        await fsPromise.access(filePath);
+        return true;
+    } catch (err) {
+        // create if not exists
+        await fsPromise.writeFile(filePath, JSON.stringify([]));
+    }
+};
 
+const validateSong = (song) => {
+    const { id, titulo, artista, tono } = song;
+    // all fields are required
+    if (
+        id.length === 0 ||
+        titulo.length === 0 ||
+        artista.length === 0 ||
+        tono.length === 0
+    ) {
+        return false;
+    }
+    return true;
+};
